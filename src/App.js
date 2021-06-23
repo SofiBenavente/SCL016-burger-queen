7e4dce3e90b98549bd02cbc3cd8e3da89a6238b0
Danipaz1 <dn.velasquezdevia@gmail.com>
2021-06-23 16:52:29 -0700

    dos cards en el home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
-import './App.css';
 import React, {Fragment, useState} from 'react';
 import logo from './Images/Burger_logo.png';
-
+import 'bootswatch/dist/journal/bootstrap.min.css'; // Added this :boom:
+import './App.css';
+import Card from './Card';
 
 function App() {
   return (
@@ -10,16 +11,7 @@ function App() {
     <header className = 'header'>
     <img src={logo} className="App-logo" alt="logo" />
     </header>
-    <div className = 'menu'>
-     <nav>
-       <p>MENU</p>
-       <ul>
-         <li>Hamburguesas</li>
-         <li>Acompañamiento</li>
-         <li>Para tomar</li>
-       </ul>
-     </nav>
-     </div>
+    <Card/>
     </div>
   );
 }
