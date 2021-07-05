7d9cce31a75bae8590dc42de934f80857919d630
SofiBenavente <sofia.benavente@gmail.com>
2021-07-04 23:59:20 -0400

    arreglo en el tamaño de botones y responsividad de app

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -30,7 +30,7 @@ const Burger = () => {
                 burgerFood.map((item,i)=> (
                     
                     <li key={i}>
-                        <button className = "burgers" onClick = {() => burgerBtnFood()}>
+                        <button className = "burgers" style={{height:400, width: 300}} onClick = {() => burgerBtnFood()}>
                         {<img src = {item.images} style={{height:200, width: 250}}/>}
                           <p><b>{item.name}</b></p>
                           <p1><i>{item.description}</i></p1>
